0cae9cd21dad25adbbd484ce295299fc996ae248
noreplydev <contact.cristiansanchez@gmail.com>
2023-07-14 20:30:26 +0200

    [FT] Added the fill of the matrix based on the cheapest path

--- index.js
+++ index.js
@@ -10,4 +10,32 @@ for (let i = 0; i < str1.length; i++) {
   } 
 }
 
-console.log(grid)
+const M = str1.length;
+const N = str2.length;
+
+// Create the matrix
+const matrix = Array(M + 1).fill(null).map(() => Array(N + 1).fill(0));
+
+// Fill the first row and column of the matrix
+for (let i = 0; i <= M; i++) {
+  matrix[i][0] = i;
+}
+for (let j = 1; j <= N; j++) {
+  matrix[0][j] = j;
+}
+
+  // Fill the rest of the matrix
+  for (let i = 1; i <= M; i++) {
+    for (let j = 1; j <= N; j++) {
+      if (str1[i - 1] === str2[j - 1]) {
+        matrix[i][j] = matrix[i - 1][j - 1];
+      } else {
+        const deletion = matrix[i - 1][j] + 1;
+        const insertion = matrix[i][j - 1] + 1;
+        const substitution = matrix[i - 1][j - 1] + 1;
+        matrix[i][j] = Math.min(deletion, insertion, substitution);
+      }
+    }
+  }
+
+console.log(matrix)
